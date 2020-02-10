52aa6677e1675cbd5cee3e1d1e07ce461d8dd762
Venkatesh Poosarla <venkatesh.poosarla@verizonmedia.com>
2020-02-09 17:01:55 -0800

    added .gitignore

--- stock/decision/StockData.py
+++ stock/decision/StockData.py
@@ -4,6 +4,7 @@ stocks = ['cmg']
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
 stock_dict = {}
 columns = ["stock", "Index", "P/E", "EPS (ttm)", "Insider Own", "Shs Outstand", "Perf Week", "Market Cap",
            "Forward P/E", "EPS next Y", "Insider Trans", "Shs Float", "Perf Month", "Income", "PEG", "EPS next Q",
@@ -14,18 +15,15 @@ columns = ["stock", "Index", "P/E", "EPS (ttm)", "Insider Own", "Shs Outstand", 
            "Sales Q/Q", "Oper. Margin", "RSI (14)", "Volatility", "Optionable", "Debt/Eq", "EPS Q/Q", "Profit Margin",
            "Rel Volume", "Prev Close", "Shortable", "LT Debt/Eq", "Earnings", "Payout", "Avg Volume", "Price", "Recom",
            "SMA20", "SMA50", "SMA200", "Volume", "Change"]
-lili=[]
+lili = []
 for stock in stocks:
-    li=[]
+    li = []
     li.append(stock)
     page = requests.get(master_url + stock)
-    # print(page.content)
     soup = BeautifulSoup(page.content, 'html.parser')
     x = soup.find_all(class_='snapshot-table2')[0]
-    # soup1 = BeautifulSoup(str(x), 'html.parser')
     rows = x.find_all(class_='table-dark-row')
     for row in rows:
-        # print("*"*20)
         cols = row.find_all('td')
         x = 1
         for col in cols:
@@ -34,9 +32,5 @@ for stock in stocks:
             x += 1
 
     lili.append(li)
-    # print (lili)
     df = pd.DataFrame(lili, columns=columns)
-df.to_csv('out.csv')          # print(col)
-    # for row in soup1.find_all(class_='table-dark-row'):
-    #     # cols=BeautifulSoup(soup1.find_all('td'))
-    #     print(row)
+df.to_csv('out.csv')
